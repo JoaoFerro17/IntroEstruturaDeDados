fa06a94c91d6cc5be91e80e3070d902ea2dc7a0f
João Victor Ferro <joaovictor.mafer@gmail.com>
2026-02-22 17:44:20 -0300

    Criação do menu e alteração no metodo exibir

--- src/ListaDinamica.java
+++ src/ListaDinamica.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ListaDinamica {
     No inicio;
 
@@ -30,11 +32,10 @@ public class ListaDinamica {
     public void exibirElementos() {
         if(existeInicio()) {
             No aux = this.inicio;
-            while(aux.getProx() != null) {
+            while(aux != null) {
                 System.out.println(aux.getConteudo());
                 aux = aux.getProx();
             }
-            System.out.println(aux.getConteudo());
         } else {
             System.out.println("Não existem elementos na Lista Dinâmica.");
         }
@@ -51,6 +52,32 @@ public class ListaDinamica {
                 }
         } else {
             System.out.println("Não existem elementos na lista.");
+        }
+    }
+
+    public void menuDeAcesso(int op){
+        Scanner sc = new Scanner(System.in);
+        String elemento;
+        switch (op){
+            case 1:
+                System.out.print("Informe o nome do produto a ser adicionado: ");
+                elemento =  sc.nextLine();
+                adicionarElemento(elemento);
+                break;
+            case 2:
+                System.out.print("Informe o nome do produto a ser removido: ");
+                elemento =  sc.nextLine();
+                removerElemento(elemento);
+                break;
+            case 3:
+                System.out.println("Lista: ");
+                exibirElementos();
+                break;
+            case 4:
+                System.out.println("Programa finalizado");
+                break;
+            default:
+                System.out.println("Opção inválida. Digite uma opção válida.");
         }
     }
 }
--- src/MainListaDinamica.java
+++ src/MainListaDinamica.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 public class MainListaDinamica {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         ListaDinamica listaDinamica = new ListaDinamica();
-        listaDinamica.adicionarElemento("Arroz");
-        listaDinamica.adicionarElemento("Feijão");
-        listaDinamica.adicionarElemento("Feijão");
-        listaDinamica.adicionarElemento("Feijão");
-        listaDinamica.adicionarElemento("Feijão");
-        listaDinamica.adicionarElemento("Feijão");
-        listaDinamica.exibirElementos();
+
+        System.out.println("---MENU DE ACESSO---");
+        int op;
+        do{
+            System.out.println("\n1- Adicionar produto\n2- Remover produto\n3- Exibir Produtos\n4- Sair do menu");
+            op = sc.nextInt();
+            listaDinamica.menuDeAcesso(op);
+        }while (op != 4);
+
+        sc.close();
     }
 }
